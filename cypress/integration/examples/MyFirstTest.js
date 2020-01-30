efcb0f40546d16b6bbf8767b4fc164553f01d149
ravindrakesar <ravi.pda@gmail.com>
2020-01-30 18:30:41 +0530

    Update MyFirstTest.js

--- cypress/integration/examples/MyFirstTest.js
+++ cypress/integration/examples/MyFirstTest.js
@@ -1,22 +1,31 @@
 
-describe('My Test Suite', function() {
-    it('Verify the title of the page-positive', function() {
+describe('Wordpress Blog Creation Test Suite', function() {
+    it('launch and verify the title of wordpress page', function() {
       cy.visit('https://wordpress.com/log-in')
       cy.title().should('eq','Log In — WordPress.com')
+      cy.wait(3000)
+    })
+
+    it('Login to wordpress website', function() {
       cy.get("#usernameOrEmail").type("ravindrakesar")
       cy.get("[type='submit']").click()
       cy.wait(3000)
       cy.get("#password").type("Shonu2014")
       cy.get("[type='submit']").click()
-      cy.wait(8000)
+      cy.wait(10000)
+    })
+
+    it('Start creating a blog', function() {
       cy.get("#header > div.masterbar__publish > a > span").click()
       cy.wait(22000)
+    })
+
+    it('Add page title and desciption', function() {
       cy.get(".editor-post-title__input").type("MyFirstBlogOnWordPress")
       cy.get("[role='textbox']").type("It feels good to write a blog")
-
-
-      
-
+      cy.wait(22000)
     })
+
+
   
   })
