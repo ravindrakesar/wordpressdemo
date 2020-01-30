75e340c8cadc23921b891049037aa4aa10a18558
ravindrakesar <ravi.pda@gmail.com>
2020-01-30 18:17:08 +0530

    Create MyFirstTest.js

---
+++ cypress/integration/examples/MyFirstTest.js
@@ -0,0 +1,22 @@
+
+describe('My Test Suite', function() {
+    it('Verify the title of the page-positive', function() {
+      cy.visit('https://wordpress.com/log-in')
+      cy.title().should('eq','Log In — WordPress.com')
+      cy.get("#usernameOrEmail").type("ravindrakesar")
+      cy.get("[type='submit']").click()
+      cy.wait(3000)
+      cy.get("#password").type("Shonu2014")
+      cy.get("[type='submit']").click()
+      cy.wait(8000)
+      cy.get("#header > div.masterbar__publish > a > span").click()
+      cy.wait(22000)
+      cy.get(".editor-post-title__input").type("MyFirstBlogOnWordPress")
+      cy.get("[role='textbox']").type("It feels good to write a blog")
+
+
+      
+
+    })
+  
+  })
